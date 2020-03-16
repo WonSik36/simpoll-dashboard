280adbc3ac93183c84c32baf76189005768f58fe
kim min ji <216000069@handong.eud>
2020-03-17 02:36:56 +0900

    0317

--- src/components/MainSpeacker.js
+++ src/components/MainSpeacker.js
@@ -8,6 +8,7 @@ import ModalBody from 'react-bootstrap/ModalBody';
 import ModalFooter from 'react-bootstrap/ModalFooter';
 import VoteList from './VoteList';
 import RoomList from './RoomList';
+import Mkroom from './Mkroom.js';
 import './style/Main.css';
 
 class MainSpeacker extends React.Component {
@@ -71,6 +72,92 @@ class MainSpeacker extends React.Component {
             )
         );
 
+        let mkroom = null;
+        let mksimpoll = null;
+        if(this.handleShow){
+            mkroom = <>
+            <Modal show={this.state.show} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Make Room</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                <Mkroom></Mkroom>
+                </Modal.Body>
+                <Modal.Footer>
+                    <div>
+                        <Button type="submit" onClick={this.handleClose}>Submit</Button>
+                    </div>
+                    <Button variant="light" onClick={this.handleClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+            </>
+        }
+        if(this.handleShow_simpoll){
+            mksimpoll=<>
+            <Modal show={this.state.show_simpoll} onHide={this.handleClose_simpoll}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Make Simpoll</Modal.Title>
+                </Modal.Header>
+                    <Modal.Body>
+                    <form action="" method="post">
+                        <div>
+                            <p>Simpoll Name</p>
+                            <input type="text" name="title" placeholder="Simpoll 이름을 입력하세요." required></input>
+                        </div>
+
+                        <div><hr />
+                            <p>URL</p>
+                            <input type="text" name="url_name" placeholder="URL을 입력하세요" required></input>
+                        </div>
+
+                        <div><hr />
+                            <p id="add_option">add option
+                                <button type="button" class="attr-btn" id="addButton" onClick={this.addChoice()}>
+                                <i class="fas fa-plus"></i>
+                                </button>
+                            </p>
+                        </div>
+
+                        {Choice_list}
+
+                        <div><hr />
+                            <table>
+
+                                <tr>
+                                    <td class="align-left">익명 투표</td>
+                                    <td><input id="anonymous_check" type="checkbox" name="anonymous_check" value={this.state.valueOne}></input></td>
+                                </tr>
+
+                                <tr>
+                                    <td class="align-left">복수 선택</td>
+                                    <td><input type="checkbox" name="vote_type" value={this.state.valueOne}></input></td>
+                                </tr>
+                        </table>
+                        </div>
+
+                        <div><hr />
+                            <p>참여 권한</p>
+                            <label><input type="radio" value={this.state.valueZero} checked /> 로그인 한 누구나</label>
+                            <label><input type="radio" value={this.state.valueOne} /> 링크를 가진 누구나</label>
+                        </div>
+
+                        <div><hr />
+                            <p id="add_option">마감시간</p>
+                            <input type="date" name="vote_end_date" value="vote_end_date" required></input>
+                            <input type="time" name="vote_end_time" value="vote_end_time" required></input>
+                        </div>
+                    </form>
+                    </Modal.Body>
+                <Modal.Footer>
+                    <div>
+                        <Button type="submit" onClick={this.handleClose_simpoll}>Submit</Button>
+                    </div>
+                    <Button variant="light" onClick={this.handleClose_simpoll}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+            </>
+        }
+
 
         return (
             <Container className="main-container">
@@ -83,42 +170,7 @@ class MainSpeacker extends React.Component {
                                     <i class="fas fa-plus"></i>
                                 </span>
                             </Button>
-
-                            {/* modal part */}
-                            <Modal show={this.state.show} onHide={this.handleClose}>
-                                <Modal.Header closeButton>
-                                    <Modal.Title>Make Room</Modal.Title>
-                                </Modal.Header>
-                                <Modal.Body>
-                                <form action="" method="post">
-                                    <div>
-                                        <p>Room Name</p>
-                                        <input type="text" name="title" placeholder="방 이름을 입력하세요." required></input>
-                                    </div>
-                                    <div><hr />
-                                        <p>URL</p>
-                                        <input type="text" name="url_name" placeholder="URL을 입력하세요" required></input>
-                                    </div>
-                                    <div><hr />
-                                        <p>Simpoll 생성 권한</p>
-                                        <label><input type="radio" value={this.state.valueZero} checked /> 방장만</label>
-                                        <label><input type="radio" value={this.state.valueOne} /> 방 참여자 모두</label>
-                                    </div>
-                                    <div><hr />
-                                        <p>참여자 실명/닉네임 여부</p>
-                                        <label><input type="radio" value={this.state.valueZero} checked /> 실명</label>
-                                        <label><input type="radio" value={this.state.valueOne} /> 닉네임</label>
-                                    </div>
-                                </form>
-                                </Modal.Body>
-                                <Modal.Footer>
-                                    <div>
-                                        <Button type="submit" onClick={this.handleClose}>Submit</Button>
-                                    </div>
-                                    <Button variant="light" onClick={this.handleClose}>Close</Button>
-                                </Modal.Footer>
-                            </Modal>
-
+                            {mkroom}
                         </div>
                         <RoomList
                             data={this.props['room-list-data']}
@@ -132,69 +184,7 @@ class MainSpeacker extends React.Component {
                                     <i class="fas fa-plus"></i>
                                 </span>
                             </Button>
-
-                            {/* modal part */}
-                            <Modal show={this.state.show_simpoll} onHide={this.handleClose_simpoll}>
-                                <Modal.Header closeButton>
-                                    <Modal.Title>Make Simpoll</Modal.Title>
-                                </Modal.Header>
-                                    <Modal.Body>
-                                    <form action="" method="post">
-                                        <div>
-                                            <p>Simpoll Name</p>
-                                            <input type="text" name="title" placeholder="Simpoll 이름을 입력하세요." required></input>
-                                        </div>
-
-                                        <div><hr />
-                                            <p>URL</p>
-                                            <input type="text" name="url_name" placeholder="URL을 입력하세요" required></input>
-                                        </div>
-
-                                        <div><hr />
-                                            <p id="add_option">add option
-                                                <button type="button" class="attr-btn" id="addButton" onClick={this.addChoice()}>
-                                                <i class="fas fa-plus"></i>
-                                                </button>
-                                            </p>
-                                        </div>
-
-                                        {Choice_list}
-
-                                        <div><hr />
-                                            <table>
-
-                                                <tr>
-                                                    <td class="align-left">익명 투표</td>
-                                                    <td><input id="anonymous_check" type="checkbox" name="anonymous_check" value={this.state.valueOne}></input></td>
-                                                </tr>
-
-                                                <tr>
-                                                    <td class="align-left">복수 선택</td>
-                                                    <td><input type="checkbox" name="vote_type" value={this.state.valueOne}></input></td>
-                                                </tr>
-                                        </table>
-                                        </div>
-
-                                        <div><hr />
-                                            <p>참여 권한</p>
-                                            <label><input type="radio" value={this.state.valueZero} checked /> 로그인 한 누구나</label>
-                                            <label><input type="radio" value={this.state.valueOne} /> 링크를 가진 누구나</label>
-                                        </div>
-
-                                        <div><hr />
-                                            <p id="add_option">마감시간</p>
-                                            <input type="date" name="vote_end_date" value="vote_end_date" required></input>
-                                            <input type="time" name="vote_end_time" value="vote_end_time" required></input>
-                                        </div>
-                                    </form>
-                                    </Modal.Body>
-                                <Modal.Footer>
-                                    <div>
-                                        <Button type="submit" onClick={this.handleClose_simpoll}>Submit</Button>
-                                    </div>
-                                    <Button variant="light" onClick={this.handleClose_simpoll}>Close</Button>
-                                </Modal.Footer>
-                            </Modal>
+                            {mksimpoll}
                         </div>
                         <VoteList
                             data={this.props['vote-list-data']}
